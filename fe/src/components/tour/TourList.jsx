a37ce923645244f8396b65e57567f6ad315f3202
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-26 16:43:36 +0900

    yb1126

---
+++ fe/src/components/tour/TourList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Flex } from "@chakra-ui/react";
+
+function TourList(props) {
+  return (
+    <div>
+      <Flex>
+        <Box>서울</Box>
+        <Box>부산</Box>
+        <Box>제주</Box>
+        <Box>강원</Box>
+        <Box>경기</Box>
+        <Box>인천</Box>
+        <Box>경상</Box>
+        <Box>충청</Box>
+        <Box>전라</Box>
+      </Flex>
+    </div>
+  );
+}
+
+export default TourList;
