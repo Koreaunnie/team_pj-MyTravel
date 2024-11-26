22db06058d831b2d8fcb4bceababc52627863c1e
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-26 17:47:56 +0900

    yb1126 투어리스트 조금2

--- fe/src/components/tour/TourList.jsx
+++ fe/src/components/tour/TourList.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import {Box, Flex} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
 
 function TourList(props) {
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const regions = [
+    "서울",
+    "부산",
+    "제주",
+    "강원",
+    "경기",
+    "인천",
+    "경상",
+    "충청",
+    "전라",
+  ];
+
   return (
     <div>
-      <Flex justify="space-between" align="center" wrap="wrap">
-        <Box onClick={}>서울</Box>
-        <Box>부산</Box>
-        <Box>제주</Box>
-        <Box>강원</Box>
-        <Box>경기</Box>
-        <Box>인천</Box>
-        <Box>경상</Box>
-        <Box>충청</Box>
-        <Box>전라</Box>
+      {/* 검색 기능 추가 */}
+      <Flex direction="column" align="center" mb={4}>
+        <Input
+          placeholder="지역을 검색하세요..."
+          value={search}
+          onChange={handleSearchChange}
+          mb={2}
+          width="300px"
+        />
+        <Flex wrap="wrap" justify="center">
+          {regions
+            .filter((region) => region.includes(search)) // 검색 필터링
+            .map((region, index) => (
+              <Box key={index} p={2} border="1px" borderColor="gray.300" m={1}>
+                <Text>{region}</Text>
+                <Button mt={2} colorScheme="teal">
+                  숙소 보기
+                </Button>
+              </Box>
+            ))}
+        </Flex>
       </Flex>
     </div>
   );
