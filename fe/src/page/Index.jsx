0735d2358cd2c4a8f77d878776da59b3572cef7c
koreaunnie <hyunjung1995@gmail.com>
2024-11-29 10:39:17 +0900

    2024.11.29 임현정: 메인 화면 CSS 디자인

--- fe/src/page/Index.jsx
+++ fe/src/page/Index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Index.css";
@@ -17,36 +17,97 @@ export function Index() {
 
   return (
     <div className={"body"}>
-      <div className={"section-wrap"}>
-        <section>
-          <div className={"section-header"}>
-            <h3>내 여행</h3>
-            <button onClick={() => navigate(`/plan/list`)}>+</button>
-          </div>
-
-          <div className={"section-body"}>
-            <ul>
-              {planList.map((plan) => (
-                <li key={plan.id}>
-                  <h5>{plan.title}</h5>
-                  <p>{plan.description}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-
-        <section>
-          <h3>투어 상품</h3>
-
-          <ul>
-            <li>
-              <h5></h5>
-              <p></p>
-            </li>
-          </ul>
-        </section>
+      <div className={"search-form-input"}>
+        <input type="text" placeholder={"어디로 떠나고 싶은가요?"} />
+        <button className={"btn-search btn-dark"}>검색</button>
       </div>
+
+      <section>
+        <div className={"section-header"}>
+          <h2>내 여행</h2>
+          <button className={"more-btn"} onClick={() => navigate(`/plan/list`)}>
+            더보기
+          </button>
+        </div>
+
+        <div className={"section-body"}>
+          <ul className={"list"}>
+            {planList.map((plan) => (
+              <li
+                key={plan.id}
+                onClick={() => navigate(`/plan/view/${plan.id}`)}
+              >
+                <h3>{plan.title}</h3>
+
+                <ul className={"list-body"}>
+                  <li className={"description"}>{plan.description}</li>
+                  <li className={"destination"}>{plan.destination}</li>
+                  <li className={"due"}>{plan.due}</li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <section>
+        <div className={"section-header"}>
+          <h2>투어 상품</h2>
+          <button className={"more-btn"} onClick={() => navigate(`/tour/list`)}>
+            더보기
+          </button>
+        </div>
+
+        <div className={"section-body"}>
+          <ul className={"list"}>
+            {planList.map((plan) => (
+              <li
+                key={plan.id}
+                onClick={() => navigate(`/plan/view/${plan.id}`)}
+              >
+                <h3>{plan.title}</h3>
+
+                <ul className={"list-body"}>
+                  <li className={"description"}>{plan.description}</li>
+                  <li className={"destination"}>{plan.destination}</li>
+                  <li className={"due"}>{plan.due}</li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <section>
+        <div className={"section-header"}>
+          <h2>커뮤니티</h2>
+          <button
+            className={"more-btn"}
+            onClick={() => navigate(`/community/list`)}
+          >
+            더보기
+          </button>
+        </div>
+
+        <div className={"section-body"}>
+          <ul className={"list"}>
+            {planList.map((plan) => (
+              <li
+                key={plan.id}
+                onClick={() => navigate(`/plan/view/${plan.id}`)}
+              >
+                <h3>{plan.title}</h3>
+
+                <ul className={"list-body"}>
+                  <li className={"description"}>{plan.description}</li>
+                  <li className={"destination"}>{plan.destination}</li>
+                  <li className={"due"}>{plan.due}</li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
