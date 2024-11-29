4192836a6eac550f8fa9e76490535bc0bcb0258e
koreaunnie <hyunjung1995@gmail.com>
2024-11-29 09:25:54 +0900

    2024.11.29 임현정: 메인 화면 section 나누는 CSS 작업 중

--- fe/src/page/Index.jsx
+++ fe/src/page/Index.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./Index.css";
 
 export function Index() {
   const [planList, setPlanList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -14,18 +17,36 @@ export function Index() {
 
   return (
     <div className={"body"}>
-      <section>
-        <h3>내 여행</h3>
+      <div className={"section-wrap"}>
+        <section>
+          <div className={"section-header"}>
+            <h3>내 여행</h3>
+            <button onClick={() => navigate(`/plan/list`)}>+</button>
+          </div>
 
-        <ul>
-          {planList.map((plan) => (
-            <li key={plan.id}>
-              <h5>{plan.title}</h5>
-              <p>{plan.description}</p>
+          <div className={"section-body"}>
+            <ul>
+              {planList.map((plan) => (
+                <li key={plan.id}>
+                  <h5>{plan.title}</h5>
+                  <p>{plan.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <section>
+          <h3>투어 상품</h3>
+
+          <ul>
+            <li>
+              <h5></h5>
+              <p></p>
             </li>
-          ))}
-        </ul>
-      </section>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
