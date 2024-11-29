a13ee5466101c17c31f69fb8beca10995ca431ec
koreaunnie <hyunjung1995@gmail.com>
2024-11-29 10:43:09 +0900

    2024.11.29 임현정: navbar 에서 member 분리

--- fe/src/components/root/Navbar.jsx
+++ fe/src/components/root/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./common.css";
+import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 // navbar 디자인
 function NavbarItem(props) {
@@ -28,13 +29,24 @@ function Navbar(props) {
 
   return (
     <nav>
-      <Flex
-        gap={3}
-        mb={4}
-        borderBottomWidth="1px"
-        borderColor="#ececec"
-        bgColor="#f9f9f9"
-      >
+      <div className={"user-container"}>
+        <ul>
+          <li onClick={() => navigate("/member/signup")}>회원가입</li>
+          <li onClick={() => navigate("/member/login")}>로그인</li>
+          <li
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/member/login");
+            }}
+          >
+            로그아웃
+          </li>
+        </ul>
+      </div>
+
+      <h1>My Travel</h1>
+
+      <div className={"nav-container"}>
         <NavbarItem onClick={() => navigate("/")}>HOME</NavbarItem>
         <NavbarItem onClick={() => navigate("/plan/list")}>내 여행</NavbarItem>
         <NavbarItem onClick={() => navigate("/tour/list")}>투어</NavbarItem>
@@ -45,21 +57,7 @@ function Navbar(props) {
         <NavbarItem onClick={() => navigate("/")}>장바구니</NavbarItem>
         <NavbarItem onClick={() => navigate("/")}>알림</NavbarItem>
         <NavbarItem onClick={() => navigate("/")}>팔로우</NavbarItem>
-        <NavbarItem onClick={() => navigate("/member/signup")}>
-          회원가입
-        </NavbarItem>
-        <NavbarItem onClick={() => navigate("/member/login")}>
-          로그인
-        </NavbarItem>
-        <NavbarItem
-          onClick={() => {
-            localStorage.removeItem("token");
-            navigate("/member/login");
-          }}
-        >
-          로그아웃
-        </NavbarItem>
-      </Flex>
+      </div>
     </nav>
   );
 }
