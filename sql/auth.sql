15e55b7493f8a5e6c60ae8a32b411d940c45e5fb
Jin19121 <jinkim19121@gmail.com>
2024-11-29 14:11:17 +0900

    권한 테이블 생성

---
+++ sql/auth.sql
@@ -0,0 +1,12 @@
+USE teamPrj1126;
+
+CREATE TABLE auth
+(
+    member_email VARCHAR(30) REFERENCES member (email),
+    auth         VARCHAR(20) NOT NULL,
+    PRIMARY KEY (member_email, auth)
+);
+
+INSERT INTO auth
+VALUES ('jejuair', 'partner');
+
