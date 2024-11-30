048a935f829c6c0976d14b6740cbcd0ea19215a1
Jin19121 <jinkim19121@gmail.com>
2024-11-30 17:51:37 +0900

    admin 권한: 멤버 목록/개인정보 접근 (에러)

--- be/src/main/java/com/example/be/controller/member/MemberController.java
+++ be/src/main/java/com/example/be/controller/member/MemberController.java
@@ -6,6 +6,7 @@ import com.example.be.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -63,11 +64,13 @@ public class MemberController {
   }
 
   @GetMapping("{email}")
+  @PreAuthorize("isAuthenticated() or hasAuthority('SCOPE_admin')")
   public Member getMember(@PathVariable String email) {
     return service.get(email);
   }
 
   @GetMapping("list")
+  @PreAuthorize("hasAuthority('SCOPE_admin')")
   public List<Member> list() {
     return service.list();
   }
