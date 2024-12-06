fd12b910b114661fe5238485132a4201033e226f
Jin19121 <jinkim19121@gmail.com>
2024-12-06 21:47:38 +0900

    jm 1206 Test 사진 업로드 확인 중 (FileList 객체)

--- be/src/main/java/com/example/be/controller/community/CommunityController.java
+++ be/src/main/java/com/example/be/controller/community/CommunityController.java
@@ -17,39 +17,42 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CommunityController {
 
-    final CommunityService service;
+  final CommunityService service;
 
-    @GetMapping("list")
-    public List<Map<String, Object>> list(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                                          @RequestParam(value = "type", defaultValue = "all") String searchType,
-                                          @RequestParam(value = "keyword", defaultValue = "") String searchKeyword) {
-        return service.list(page, searchType, searchKeyword);
-    }
+  @GetMapping("list")
+  public List<Map<String, Object>> list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                        @RequestParam(value = "type", defaultValue = "all") String searchType,
+                                        @RequestParam(value = "keyword", defaultValue = "") String searchKeyword) {
+    return service.list(page, searchType, searchKeyword);
+  }
 
 
-    @PostMapping("write")
-    public void write(Community community, @RequestParam(value = "files[]", required = false) MultipartFile[] files, Authentication auth) {
+  @PostMapping("write")
+  public void write(
+          Community community,
+          @RequestParam(value = "files[]", required = false) MultipartFile[] files,
+          Authentication auth) {
+    System.out.println(files);
+    service.write(community, files, auth);
+  }
 
-        service.write(community, files, auth);
-    }
+  @GetMapping("view/{id}")
+  public Map<String, Object> view(@PathVariable Integer id) {
+    return service.view(id);
+  }
 
-    @GetMapping("view/{id}")
-    public Map<String, Object> view(@PathVariable Integer id) {
-        return service.view(id);
-    }
+  @PutMapping("edit")
+  public void edit(@RequestBody Community community) {
+    System.out.println(community);
+    service.edit(community);
+  }
 
-    @PutMapping("edit")
-    public void edit(@RequestBody Community community) {
-        System.out.println(community);
-        service.edit(community);
-    }
+  @DeleteMapping("delete/{id}")
+  public void delete(@PathVariable Integer id) {
+    service.delete(id);
+  }
 
-    @DeleteMapping("delete/{id}")
-    public void delete(@PathVariable Integer id) {
-        service.delete(id);
-    }
-
-    // TODO : UPDATE 기능 추가
+  // TODO : UPDATE 기능 추가
 
 
 }
--- be/src/main/java/com/example/be/service/community/CommunityService.java
+++ be/src/main/java/com/example/be/service/community/CommunityService.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,83 +24,82 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CommunityService {
 
-    final CommunityMapper mapper;
-    final S3Client s3;
+  final CommunityMapper mapper;
+  final S3Client s3;
 
 
-    @Value("${image.src.prefix}")
-    String imageSrcPrefix;
+  @Value("${image.src.prefix}")
+  String imageSrcPrefix;
 
-    @Value("${bucket.name}")
-    String bucketName;
+  @Value("${bucket.name}")
+  String bucketName;
 
-    public List<Map<String, Object>> list(Integer page, String searchType, String searchKeyword) {
+  public List<Map<String, Object>> list(Integer page, String searchType, String searchKeyword) {
 
-        Integer pageList = (page - 1) * 10;
+    Integer pageList = (page - 1) * 10;
 
-        return mapper.listUp(pageList, searchType, searchKeyword);
-    }
+    return mapper.listUp(pageList, searchType, searchKeyword);
+  }
 
-    public void write(Community community, MultipartFile[] files, Authentication auth) {
+  public void write(Community community, MultipartFile[] files, Authentication auth) {
 
+    String nickname = mapper.findNickname(auth.getName());
+    community.setWriter(nickname);
 
-        String nickname = mapper.findNickname(auth.getName());
-        community.setWriter(nickname);
-        mapper.writeCommunity(community);
-        Integer id = community.getId();
+    mapper.writeCommunity(community);
+    Integer id = community.getId();
 
-        if (files != null && files.length > 0) {
+    if (files != null && files.length > 0) {
 
-            String directory = STR."C:/Temp/teamPrj1126/\{id}";
-            File dir = new File(directory);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
+      for (MultipartFile file : files) {
 
-            // 파일 업로드
-            // TODO: local -> aws
+        String fileName = file.getOriginalFilename();
+        String filePath = STR."teamPrj1126/community/\{community.getId()}/\{fileName}";
+        PutObjectRequest por = PutObjectRequest
+                .builder()
+                .bucket(bucketName)
+                .key(filePath)
+                .acl(ObjectCannedACL.PUBLIC_READ)
+                .build();
 
-            for (MultipartFile file : files) {
-
-                String fileName = file.getOriginalFilename();
-                String filePath = STR."C:/Temp/teamPrj1126/\{community.getId()}/\{fileName}";
-                try {
-                    file.transferTo(new File(filePath));
-                    mapper.addFile(fileName, id);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        try {
+          s3.putObject(por, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+          throw new RuntimeException(e);
         }
 
-
+        mapper.addFile(fileName, id);
+      }
     }
 
-    public Map<String, Object> view(Integer id) {
 
-        Map<String, Object> viewer = mapper.viewCommunity(id);
-        List<String> fileList = mapper.callCommunityFile(id);
-        if (fileList.size() != 0) {
-            List<Object> files = new ArrayList();
-            for (String fileName : fileList) {
-                Map<String, Object> file = new HashMap<>();
-                String filePath = STR."\{imageSrcPrefix}/\{viewer.get("id").toString()}/\{fileName}";
-                file.put("fileName", fileName);
-                file.put("filePath", filePath);
-                files.add(file);
-            }
-            viewer.put("files", files);
-            return viewer;
-        } else {
-            return viewer;
-        }
-    }
+  }
 
-    public void edit(Community community) {
-        mapper.editCommunity(community);
-    }
+  public Map<String, Object> view(Integer id) {
 
-    public void delete(Integer id) {
-        mapper.deleteCommunity(id);
+    Map<String, Object> viewer = mapper.viewCommunity(id);
+    List<String> fileList = mapper.callCommunityFile(id);
+    if (fileList.size() != 0) {
+      List<Object> files = new ArrayList();
+      for (String fileName : fileList) {
+        Map<String, Object> file = new HashMap<>();
+        String filePath = STR."\{imageSrcPrefix}/\{viewer.get("id").toString()}/\{fileName}";
+        file.put("fileName", fileName);
+        file.put("filePath", filePath);
+        files.add(file);
+      }
+      viewer.put("files", files);
+      return viewer;
+    } else {
+      return viewer;
     }
+  }
+
+  public void edit(Community community) {
+    mapper.editCommunity(community);
+  }
+
+  public void delete(Integer id) {
+    mapper.deleteCommunity(id);
+  }
 }
