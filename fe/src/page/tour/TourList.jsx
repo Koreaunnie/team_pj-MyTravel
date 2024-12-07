ca19b0a8a80e07bec3d6aa390171a305bd96177c
Jin19121 <jinkim19121@gmail.com>
2024-12-07 15:36:50 +0900

    jm 1207 Feat Tour 글쓰기 버튼 추가 - partner 혹은 admin 한정

--- fe/src/page/tour/TourList.jsx
+++ fe/src/page/tour/TourList.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Box, Center, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Breadcrumb } from "../../components/root/Breadcrumb.jsx";
+import { AuthenticationContext } from "../../components/context/AuthenticationProvider.jsx";
 
 function TourList() {
   const [tourList, setTourList] = useState([]);
@@ -11,6 +12,7 @@ function TourList() {
     type: searchParams.get("type") ?? "all",
     keyword: searchParams.get("key") ?? "",
   });
+  const { isPartner, isAdmin } = useContext(AuthenticationContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -57,68 +59,78 @@ function TourList() {
         depth1={"Tour 목록"}
         navigateToDepth1={() => navigate(`/tour/list`)}
       />
-      <h1>Tour 목록</h1>
-
-      <Center>
-        <div className={"search-form"}>
-          <select
-            value={search.type}
-            onChange={(e) => setSearch({ ...search, type: e.target.value })}
+      <div>
+        {(isPartner || isAdmin) && (
+          <button
+            className={"btn btn-dark"}
+            onClick={() => navigate(`/tour/add`)}
           >
-            <option value="all">전체</option>
-            <option value="title">제목</option>
-            <option value="product">제품</option>
-            <option value="location">위치</option>
-            <option value="content">본문</option>
-            <option value="partner">파트너사</option>
-          </select>
-          <div className={"search-form-input"}>
-            <input
-              type={"search"}
-              value={search.keyword}
-              onChange={(e) =>
-                setSearch({ ...search, keyword: e.target.value.trim() })
-              }
-            />
-            <button
-              className={"btn-search btn-dark"}
-              onClick={handleSearchClick}
-            >
-              검색
-            </button>
-          </div>
-        </div>
-      </Center>
+            새 상품 등록
+          </button>
+        )}
+        <h1>Tour 목록</h1>
 
-      {tourList.length === 0 ? (
-        <p>찾으시는 상품이 존재하지 않습니다.</p>
-      ) : (
-        <SimpleGrid
-          columns={{ base: 2, md: 4, lg: 5, xl: 6, "2xl": 7 }}
-          spacing={6}
-        >
-          {tourList.map((tour) => (
-            <Box
-              key={tour.id}
-              borderWidth={"1px"}
-              borderRadius={"1g"}
-              overflow={"hidden"}
-              p={4}
-              m={1}
-              _hover={{ boxShadow: "1g" }}
-              onClick={() => handleRowClick(tour.id)}
+        <Center>
+          <div className={"search-form"}>
+            <select
+              value={search.type}
+              onChange={(e) => setSearch({ ...search, type: e.target.value })}
             >
-              <Image key={tour.image} src={tour.src} />
-              <Text>
-                <b>{tour.title}</b>
-              </Text>
-              <Text>{tour.location}</Text>
-              <Text>{tour.product}</Text>
-              <Text>{tour.price}</Text>
-            </Box>
-          ))}
-        </SimpleGrid>
-      )}
+              <option value="all">전체</option>
+              <option value="title">제목</option>
+              <option value="product">제품</option>
+              <option value="location">위치</option>
+              <option value="content">본문</option>
+              <option value="partner">파트너사</option>
+            </select>
+            <div className={"search-form-input"}>
+              <input
+                type={"search"}
+                value={search.keyword}
+                onChange={(e) =>
+                  setSearch({ ...search, keyword: e.target.value.trim() })
+                }
+              />
+              <button
+                className={"btn-search btn-dark"}
+                onClick={handleSearchClick}
+              >
+                검색
+              </button>
+            </div>
+          </div>
+        </Center>
+
+        {tourList.length === 0 ? (
+          <p>찾으시는 상품이 존재하지 않습니다.</p>
+        ) : (
+          <SimpleGrid
+            columns={{ base: 2, md: 4, lg: 5, xl: 6, "2xl": 7 }}
+            spacing={6}
+          >
+            {tourList.map((tour) => (
+              <Box
+                key={tour.id}
+                borderWidth={"1px"}
+                borderRadius={"1g"}
+                overflow={"hidden"}
+                p={4}
+                m={1}
+                _hover={{ boxShadow: "1g" }}
+                onClick={() => handleRowClick(tour.id)}
+              >
+                <Image key={tour.image} src={tour.src} />
+                <Text>
+                  <b>{tour.title}</b>
+                </Text>
+                <Text>{tour.location}</Text>
+                <Text>{tour.product}</Text>
+                <Text>{tour.price}</Text>
+              </Box>
+            ))}
+          </SimpleGrid>
+        )}
+      </div>
     </div>
   );
 }
