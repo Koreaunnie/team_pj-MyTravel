9c4941f636c29d0480068661236fb8995bcc4b13
Jin19121 <jinkim19121@gmail.com>
2024-12-09 18:43:09 +0900

    1209 김지민 Design: tourList 박스형/목록형 나눔

--- fe/src/page/tour/TourList.jsx
+++ fe/src/page/tour/TourList.jsx
@@ -7,6 +7,7 @@ import { AuthenticationContext } from "../../components/context/AuthenticationPr
 
 function TourList() {
   const [tourList, setTourList] = useState([]);
+  const [selectedMenu, setSelectedMenu] = useState("box");
   const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState({
     type: searchParams.get("type") ?? "all",
@@ -53,12 +54,17 @@ function TourList() {
     }
   }
 
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
+
   return (
     <div className={"tour"}>
       <Breadcrumb
         depth1={"Tour 목록"}
         navigateToDepth1={() => navigate(`/tour/list`)}
       />
+
       <div>
         {(isPartner || isAdmin) && (
           <button
@@ -71,6 +77,7 @@ function TourList() {
         <h1>Tour 목록</h1>
 
         <Center>
+          {/*검색*/}
           <div className={"search-form"}>
             <select
               value={search.type}
@@ -101,36 +108,83 @@ function TourList() {
           </div>
         </Center>
 
-        {tourList.length === 0 ? (
-          <p>찾으시는 상품이 존재하지 않습니다.</p>
-        ) : (
-          <SimpleGrid
-            columns={{ base: 2, md: 4, lg: 5, xl: 6, "2xl": 7 }}
-            spacing={6}
-          >
-            {tourList.map((tour) =>
-              tour.active ? (
-                <Box
-                  key={tour.id}
-                  borderWidth={"1px"}
-                  borderRadius={"1g"}
-                  overflow={"hidden"}
-                  p={4}
-                  m={1}
-                  _hover={{ boxShadow: "1g" }}
-                  onClick={() => handleRowClick(tour.id)}
-                >
-                  <Image key={tour.image} src={tour.src} />
-                  <Text>
-                    <b>{tour.title}</b>
-                  </Text>
-                  <Text>{tour.location}</Text>
-                  <Text>{tour.product}</Text>
-                  <Text>{tour.price}</Text>
-                </Box>
-              ) : null,
+        {/* 보기 선택 */}
+        <button
+          className={"btn btn-dark-outline"}
+          onClick={() => handleMenuClick("box")}
+        >
+          박스형
+        </button>
+        <button
+          className={"btn btn-dark-outline"}
+          onClick={() => handleMenuClick("list")}
+        >
+          목록형
+        </button>
+
+        {selectedMenu === "box" && (
+          <div>
+            {tourList.length === 0 ? (
+              <p>찾으시는 상품이 존재하지 않습니다.</p>
+            ) : (
+              <SimpleGrid
+                columns={{ base: 2, md: 3, lg: 4, xl: 5, "2xl": 6 }}
+                spacing={6}
+              >
+                {tourList.map((tour) =>
+                  tour.active ? (
+                    <Box
+                      key={tour.id}
+                      borderWidth={"1px"}
+                      borderRadius={"1g"}
+                      overflow={"hidden"}
+                      p={4}
+                      m={1}
+                      _hover={{ boxShadow: "1g" }}
+                      onClick={() => handleRowClick(tour.id)}
+                    >
+                      <Image key={tour.image} src={tour.src} />
+                      <Text>
+                        <b>{tour.title}</b>
+                      </Text>
+                      <Text>{tour.location}</Text>
+                      <Text>{tour.product}</Text>
+                      <Text>{tour.price}</Text>
+                    </Box>
+                  ) : null,
+                )}
+              </SimpleGrid>
             )}
-          </SimpleGrid>
+          </div>
+        )}
+        {selectedMenu === "list" && (
+          <div>
+            {tourList.length === 0 ? (
+              <p>찾으시는 상품이 존재하지 않습니다.</p>
+            ) : (
+              <table className={"table-list"}>
+                <tbody>
+                  {tourList.map((tour) =>
+                    tour.active ? (
+                      <tr
+                        key={tour.id}
+                        _hover={{ boxShadow: "1g" }}
+                        onClick={() => handleRowClick(tour.id)}
+                      >
+                        <td>
+                          <Image key={tour.image} src={tour.src} />
+                        </td>
+                        <td>{tour.title}</td>
+                        <td>{tour.location}</td>
+                        <td>{tour.product}</td>
+                        <td>{tour.price}</td>
+                      </tr>
+                    ) : null,
+                  )}
+                </tbody>
+              </table>
+            )}
+          </div>
         )}
       </div>
     </div>
