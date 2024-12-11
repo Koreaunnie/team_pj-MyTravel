7b0bd786c8703767db67f13b721331f3c9784e73
koreaunnie <hyunjung1995@gmail.com>
2024-12-11 11:29:34 +0900

    1211 임현정 Fix: 문의 게시판 수정, 삭제 버튼은 로그인 한 사용자가 쓴 글에만 보이게 (writer 와 로그인한 사용자 email 대조하던 걸 nickname 대조로 변경)

--- fe/src/page/cs/inquiry/InquiryView.jsx
+++ fe/src/page/cs/inquiry/InquiryView.jsx
@@ -16,7 +16,7 @@ function InquiryView(props) {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const { hasAccess } = useContext(AuthenticationContext);
+  const { nickname } = useContext(AuthenticationContext);
 
   useEffect(() => {
     axios.get(`/api/cs/inquiry/view/${id}`).then((res) => {
@@ -69,7 +69,7 @@ function InquiryView(props) {
             목록
           </button>
 
-          {hasAccess(inquiry.writer) && (
+          {nickname === inquiry.writer && (
             <button
               type={"button"}
               className={"btn btn-dark"}
@@ -79,7 +79,7 @@ function InquiryView(props) {
             </button>
           )}
 
-          {hasAccess(inquiry.writer) && (
+          {nickname === inquiry.writer && (
             <button
               type={"button"}
               className={"btn btn-warning"}
