d955d08abff6366a3e3645c6d02c4dbbb078e579
koreaunnie <hyunjung1995@gmail.com>
2024-12-11 16:53:51 +0900

    1211 임현정 Design: 고객센터 문의 게시판 날짜 형식 YYYY-MM-DD 로 렌더링

--- fe/src/page/cs/CsIndex.jsx
+++ fe/src/page/cs/CsIndex.jsx
@@ -33,6 +33,16 @@ function CsIndex(props) {
     }
   };
 
+  // 날짜 포맷팅
+  const formattedDate = (props) => {
+    const date = new Date(props);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1 해줘야 함
+    const day = String(date.getDate()).padStart(2, "0"); // 두 자릿수로 맞추기 위해 padStart 사용
+
+    return `${year}-${month}-${day}`;
+  };
+
   return (
     <div className={"cs"}>
       <Breadcrumb
@@ -90,7 +100,7 @@ function CsIndex(props) {
                   ) : (
                     <li>{inquiry.title}</li>
                   )}
-                  <li className={"date"}>{inquiry.inserted}</li>
+                  <li className={"date"}>{formattedDate(inquiry.inserted)}</li>
                 </ul>
               ))}
             </section>
--- fe/src/page/cs/inquiry/InquiryList.jsx
+++ fe/src/page/cs/inquiry/InquiryList.jsx
@@ -37,6 +37,16 @@ function InquiryList(props) {
     }
   };
 
+  // 날짜 포맷팅
+  const formattedDate = (props) => {
+    const date = new Date(props);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1 해줘야 함
+    const day = String(date.getDate()).padStart(2, "0"); // 두 자릿수로 맞추기 위해 padStart 사용
+
+    return `${year}-${month}-${day}`;
+  };
+
   return (
     <div className={"inquiry"}>
       <Breadcrumb
@@ -90,7 +100,7 @@ function InquiryList(props) {
                   <td className={"title-center"}>{inquiry.title}</td>
                 )}
                 <td>{inquiry.writerNickname}</td>
-                <td>{inquiry.updated}</td>
+                <td>{formattedDate(inquiry.updated)}</td>
               </tr>
             ))}
           </tbody>
