bf126301682d41f258c1a6ee630e19c191534140
Jin19121 <jinkim19121@gmail.com>
2024-12-11 09:26:23 +0900

    1210 김지민 Feat: 카카오 로그인 작업 중

--- fe/src/components/login/SocialKakao.jsx
+++ fe/src/components/login/SocialKakao.jsx
@@ -22,30 +22,33 @@ const SocialKakao = () => {
       data.append("redirect_uri", "http://localhost:5173"); // 리다이렉트 URI
       data.append("code", code); // 인가 코드
 
+      //카카오에 토큰 요청
       fetch("https://kauth.kakao.com/oauth/token", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
         },
+        //요청 본문
         body: data.toString(),
       })
         .then((res) => res.json())
         .then((tokenData) => {
           console.log("토큰응답", tokenData);
-          if (tokenData.accessToken) {
+          if (tokenData.access_token) {
             //accessToken 저장
-            console.log("액세스 토큰:", tokenData.accessToken);
-            localStorage.setItem("accessToken", tokenData.accessToken);
+            console.log("액세스 토큰:", tokenData.access_token);
+            localStorage.setItem("accessToken", tokenData.access_token);
 
             //추가처리
             fetch(`/api/member/login/kakao`, {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${tokenData.accessToken}`,
+                Authorization: `Bearer ${tokenData.access_token}`,
               },
               body: JSON.stringify({
-                accessToken: tokenData.accessToken,
+                accessToken: tokenData.access_token,
+                refreshToken: tokenData.resresh_token,
               }),
             })
               .then((r) => r.json())
@@ -53,6 +56,8 @@ const SocialKakao = () => {
                 console.log("사용자 데이터", userData);
               })
               .catch((error) => console.error("백엔드 호출 실패: ", error));
+          } else {
+            console.error("token 요청 실패: ", tokenData);
           }
           //로그인 처리 로직(토큰 저장, redirect)
         })
