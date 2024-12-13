3fdfd7a710755fd6a73cb9748b6d9ccabf05a69d
Lemonstew <colt4949@naver.com>
2024-12-13 14:19:08 +0900

    1213 민재원 Feat: 좋아요 중

---
+++ be/src/main/java/com/example/be/dto/community/CommunityLike.java
@@ -0,0 +1,9 @@
+package com.example.be.dto.community;
+
+import lombok.Data;
+
+@Data
+public class CommunityLike {
+    private Integer communityId;
+    private String nickName;
+}
--- fe/src/page/community/CommunityView.jsx
+++ fe/src/page/community/CommunityView.jsx
@@ -80,6 +80,12 @@ function CommunityView(props) {
       .catch((err) => console.error(err));
   };
 
+  const fetchLike = () => {
+    axios.get(`api/community/view/${id}`).then((res) => {
+      setMyCommunityLike();
+    });
+  };
+
   const handleCommentSaveClick = () => {
     axios
       .post(`/api/community/comment/write`, {
@@ -111,6 +117,22 @@ function CommunityView(props) {
       .catch((err) => console.error(err));
   };
 
+  // TODO: 로그인에 대한 권한 완료 후 좋아요 즉시 반영 시도하기
+  // const handleLikeClick = () => {
+  //   setMyCommunityLike(!myCommunityLike);
+  //   if (myCommunityLike) {
+  //     axios
+  //       .post(`/api/community/like/${id}`, {
+  //         like: myCommunityLike,
+  //       })
+  //       .then(() => fetchComments());
+  //   } else {
+  //     axios
+  //       .delete(`/api/community/like/delete?id=${id}&nickName=${}`)
+  //       .then(() => fetchLike());
+  //   }
+  // };
+
   return (
     <div>
       <Breadcrumb
@@ -135,11 +157,7 @@ function CommunityView(props) {
             </Field>
             <Field>
               <Stack>
-                <Icon
-                  fontSize="8xl"
-                  color="red.600"
-                  onClick={() => setMyCommunityLike(!myCommunityLike)}
-                >
+                <Icon fontSize="8xl" color="red.600" onClick={handleLikeClick}>
                   {myCommunityLike ? <IoMdHeart /> : <IoMdHeartEmpty />}
                 </Icon>
                 <h5>{community.like}</h5>
