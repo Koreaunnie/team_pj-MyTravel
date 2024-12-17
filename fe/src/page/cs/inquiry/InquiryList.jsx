8c3a8c229795306c8dc5c0e4787c214b20869aae
koreaunnie <hyunjung1995@gmail.com>
2024-12-17 19:40:06 +0900

    1217 임현정 Feat: 고객센터 > 문의하기 페이지네이션과 검색 추가

--- fe/src/page/cs/inquiry/InquiryList.jsx
+++ fe/src/page/cs/inquiry/InquiryList.jsx
@@ -1,24 +1,69 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Breadcrumb } from "../../../components/root/Breadcrumb.jsx";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Modal } from "../../../components/root/Modal.jsx";
 import { AuthenticationContext } from "../../../components/context/AuthenticationProvider.jsx";
 import { CiLock } from "react-icons/ci";
 import { toaster } from "../../../components/ui/toaster.jsx";
+import {
+  PaginationItems,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "../../../components/ui/pagination.jsx";
+import { HStack } from "@chakra-ui/react";
 
 function InquiryList(props) {
   const { nickname, isAuthenticated } = useContext(AuthenticationContext);
   const [inquiryList, setInquiryList] = useState([]);
   const [addModalOpen, setAddModalOpen] = useState(false);
   const [showMyInquiries, setShowMyInquiries] = useState(false); // 내가 쓴 글 여부
+  const [search, setSearch] = useState({ type: "all", keyword: "" });
+  const [count, setCount] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("/api/cs/inquiry/list").then((res) => {
-      setInquiryList(res.data);
-    });
-  }, []);
+    axios
+      .get("/api/cs/inquiry/list", {
+        params: {
+          st: search.type,
+          sk: search.keyword,
+          page: pageParam,
+        },
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        setInquiryList(data.list);
+        setCount(data.count);
+      });
+  }, [searchParams]);
+
+  useEffect(() => {
+    const nextSearch = { ...search };
+
+    if (searchParams.get("st")) {
+      nextSearch.type = searchParams.get("st");
+    } else {
+      nextSearch.type = "all";
+    }
+
+    if (searchParams.get("sk")) {
+      nextSearch.keyword = searchParams.get("sk");
+    } else {
+      nextSearch.keyword = "";
+    }
+
+    setSearch(nextSearch);
+  }, [searchParams]);
+
+  // 엔터 키로 검색 실행
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      handleSearchButton();
+    }
+  }
 
   // 로그인 안 했을 때 작성 버튼 클릭하면 로그인 유도
   const checkLoginOrNot = () => {
@@ -58,6 +103,33 @@ function InquiryList(props) {
 
     return `${year}-${month}-${day}`;
   };
+
+  const handleSearchButton = () => {
+    const nextSearchParam = new URLSearchParams(searchParams);
+
+    if (search.keyword.trim().length > 0) {
+      nextSearchParam.set("st", search.type);
+      nextSearchParam.set("sk", search.keyword);
+    } else {
+      nextSearchParam.delete("st");
+      nextSearchParam.delete("sk");
+    }
+
+    setSearchParams(nextSearchParam);
+  };
+
+  // pagination
+  // page 번호 (searchParams : URL 쿼리 파라미터 관리)
+  const pageParam = searchParams.get("page") ?? "1";
+  // 문자열로 가져온 page 값을 숫자 타입으로 변환
+  const page = Number(pageParam);
+
+  // 페이지 번호 변경 시 URL 의 쿼리 파라미터를 업데이트
+  function handlePageChange(e) {
+    const nextSearchParams = new URLSearchParams(searchParams);
+    nextSearchParams.set("page", e.page);
+    setSearchParams(nextSearchParams);
+  }
 
   return (
     <div className={"inquiry"}>
@@ -104,25 +176,84 @@ function InquiryList(props) {
           </thead>
 
           <tbody>
-            {filteredInquiries.map((inquiry) => (
-              <tr key={inquiry.id} onClick={() => checkSecretOrNot(inquiry)}>
-                <td>{inquiry.id}</td>
-                {inquiry.secret ? (
-                  <td className={"title-center secret"}>
-                    <span className={"icon"}>
-                      <CiLock />
-                    </span>
-                    비밀글입니다.
-                  </td>
-                ) : (
-                  <td className={"title-center"}>{inquiry.title}</td>
-                )}
-                <td>{inquiry.writerNickname}</td>
-                <td>{formattedDate(inquiry.updated)}</td>
-              </tr>
-            ))}
+            {filteredInquiries.length > 0 ? (
+              filteredInquiries.map((inquiry) => (
+                <tr key={inquiry.id} onClick={() => checkSecretOrNot(inquiry)}>
+                  <td>{inquiry.id}</td>
+                  {inquiry.secret ? (
+                    <td className={"title-center secret"}>
+                      <span className={"icon"}>
+                        <CiLock />
+                      </span>
+                      비밀글입니다.
+                    </td>
+                  ) : (
+                    <td className={"title-center"}>{inquiry.title}</td>
+                  )}
+                  <td>{inquiry.writerNickname}</td>
+                  <td>{formattedDate(inquiry.updated)}</td>
+                </tr>
+              ))
+            ) : (
+              <td colSpan={4}>
+                <div className={"empty-container"}>
+                  <p className={"empty-container-title"}>
+                    검색 결과가 없습니다.
+                  </p>
+                  <p className={"empty-container-description"}>
+                    검색어를 확인해주세요.
+                  </p>
+                </div>
+              </td>
+            )}
           </tbody>
         </table>
+
+        {/* 검색 */}
+        <div className={"search-form"}>
+          <select
+            onChange={(e) => setSearch({ ...search, type: e.target.value })}
+          >
+            <option value="all">전체</option>
+            <option value="title">제목</option>
+            <option value="content">내용</option>
+            <option value="writer">작성자</option>
+          </select>
+
+          <div className={"search-form-input"}>
+            <input
+              type="search"
+              value={search.keyword}
+              onChange={(e) =>
+                setSearch({ ...search, keyword: e.target.value.trim() })
+              }
+              onKeyPress={handleKeyPress}
+            />
+            <button
+              className={"btn-search btn-dark"}
+              onClick={handleSearchButton}
+            >
+              검색
+            </button>
+          </div>
+        </div>
+
+        {/* pagination */}
+        <div className="pagination">
+          <PaginationRoot
+            onPageChange={handlePageChange}
+            count={count}
+            pageSize={10}
+            page={page}
+            variant="solid"
+          >
+            <HStack>
+              <PaginationPrevTrigger />
+              <PaginationItems />
+              <PaginationNextTrigger />
+            </HStack>
+          </PaginationRoot>
+        </div>
       </div>
 
       {/* 추가 modal */}
