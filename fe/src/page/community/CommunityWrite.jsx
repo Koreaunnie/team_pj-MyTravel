d88e3ee40144fda09bb833680aae8cf41bcdd0fe
Lemonstew <colt4949@naver.com>
2024-12-18 16:38:22 +0900

    1218 민재원 Feat: 커뮤니티 작성(토스터)

--- fe/src/page/community/CommunityWrite.jsx
+++ fe/src/page/community/CommunityWrite.jsx
@@ -13,6 +13,7 @@ import { HiUpload } from "react-icons/hi";
 import { AuthenticationContext } from "../../components/context/AuthenticationProvider.jsx";
 import MemberLogin from "../member/MemberLogin.jsx";
 import { Alert } from "../../components/ui/alert.jsx";
+import { toaster } from "../../components/ui/toaster.jsx";
 
 function CommunityWrite(props) {
   const [title, setTitle] = useState("");
@@ -25,7 +26,22 @@ function CommunityWrite(props) {
     axios
       .postForm(`/api/community/write`, { title, content, files })
       .then((e) => {
+        const writeSuccess = e.data.message;
+        toaster.create({
+          type: writeSuccess.type,
+          description: writeSuccess.text,
+        });
         navigate(`/community/view/${e.data.id}`);
+      })
+      .catch((e) => {
+        const writeFailure = e.request.response;
+        const parsingKey = JSON.parse(writeFailure);
+        const type = parsingKey.message.type;
+        const text = parsingKey.message.text;
+        toaster.create({
+          type: type,
+          description: text,
+        });
       });
   };
 
