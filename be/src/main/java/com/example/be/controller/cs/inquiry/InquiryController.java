21a32168d40d3b116c8f35b1200e9ecd981f5e9f
Jin19121 <jinkim19121@gmail.com>
2024-12-24 16:07:53 +0900

    1224 김지민 Test: 테스트용 sout 삭제

--- be/src/main/java/com/example/be/controller/cs/inquiry/InquiryController.java
+++ be/src/main/java/com/example/be/controller/cs/inquiry/InquiryController.java
@@ -110,12 +110,10 @@ public class InquiryController {
 
         if (service.delete(id)) {
             // 성공
-            System.out.println("성공");
             return ResponseEntity.ok(Map.of("message", Map.of(
                 "type", "success", "text", "문의 글이 삭제되었습니다.")));
         } else {
             // 실패
-            System.out.println("실패");
             return ResponseEntity.badRequest().body(Map.of("message", Map.of(
                 "type", "warning", "text", "삭제 중 오류가 생겼습니다.")));
         }
